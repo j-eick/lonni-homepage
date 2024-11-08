73f732f524fd4410f9d087a8c9319a376e365eb3
Joschka Eickhoff <jeickhoff@posteo.de>
2024-11-08 17:09:36 +0100

    outsourced navLinks

---
+++ lib/navLinks.ts
@@ -0,0 +1,10 @@
+const links = [
+    { href: "/home", name: "Home" },
+    { href: "/home/ausbildung", name: "Ausbildung" },
+    { href: "/ausbildung", name: "Künstlerische Tätigkeit" },
+    { href: "/ausbildung", name: "Pädagogische Erfahrung" },
+    { href: "/ausbildung", name: "Links" },
+    { href: "/ausbildung", name: "Kontakt" },
+];
+
+export default links;
