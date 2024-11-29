e8301fb3547a827c22126847a88c0771f267f210
Joschka Eickhoff <jeickhoff@posteo.de>
2024-11-29 23:35:35 +0100

    ui / navItem positioning

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -19,25 +19,63 @@ export default function Home() {
     }, []);
 
     return (
-        <div className="h-full w-full grid grid-cols-12 grid-rows-12">
+        <div className="br h-full w-9/10 mx-auto my-0 grid grid-cols-12 grid-rows-12">
             <p className={`absolute top-2 right-2`}>{windowSize.width}px</p>
 
             <aside
                 className={`bb 
                             w-full
                             col-start-1 col-end-6
-                            row-start-2 row-end-11
+                            row-start-2 row-end-12
                             `}
             >
-                <nav className={`h-full br grid grid-cols-8 grid-rows-12`}>
-                    <Link href={""}>Leidenschaft</Link>
-                    <Link href={""}>Unterricht</Link>
-                    <Link href={""}>Künsterlische Tätigkeiten</Link>
-                    <Link href={""}>Kontakt</Link>
+                <nav
+                    className={`h-full grid grid-cols-12 grid-rows-16`}
+                    id="startpage-navItems"
+                >
+                    <Link
+                        href={""}
+                        className={`row-start-2 row-end-6
+                                    col-start-1 col-end-7`}
+                    >
+                        Leidenschaft
+                    </Link>
+                    <Link
+                        href={""}
+                        className={`z-10
+                                    col-start-6 col-end-12
+                                    row-start-4 row-end-8
+                                    `}
+                    >
+                        Unterricht
+                    </Link>
+                    <Link
+                        href={""}
+                        className={`col-start-3 col-end-9
+                                    row-start-7 row-end-11`}
+                    >
+                        Künsterlische Tätigkeiten
+                    </Link>
+                    <Link
+                        href={""}
+                        className={`bb bg-blue-400
+                                    col-start-2 col-end-7
+                                    row-start-12 row-end-15`}
+                    >
+                        Auftritte
+                    </Link>
+                    <Link
+                        href={""}
+                        className={`bb bg-slate-400
+                                    col-start-6 col-end-11
+                                    row-start-14 row-end-17`}
+                    >
+                        Kontakt
+                    </Link>
                 </nav>
             </aside>
             <h1
-                className={`col-start-5 col-end-13       
+                className={`br col-start-5 col-end-13
                             row-start-1 row-end-3`}
             >
                 Lonni Inman
@@ -45,7 +83,7 @@ export default function Home() {
             <main
                 className={`bb 
                             col-start-6 col-end-13
-                            row-start-2 row-end-11`}
+                            row-start-2 row-end-12`}
             >
                 <article></article>
             </main>
