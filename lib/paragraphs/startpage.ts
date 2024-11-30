58deff0147d7d6c9eb0c3b1d68db094ccda4ac9e
Joschka Eickhoff <jeickhoff@posteo.de>
2024-11-30 01:24:23 +0100

    ui / adding welcomeText

---
+++ lib/paragraphs/startpage.ts
@@ -0,0 +1,15 @@
+type StartPageProps = {
+    welcomeText: string;
+};
+
+export const startpage: StartPageProps = {
+    welcomeText: `Ich freue mich, meine Leidenschaft für Musik mit Ihnen zu teilen – sei es in individuellen Unterrichtsstunden, spannenden Projekten oder besonderen Konzerterlebnissen.
+
+Mein Unterricht ist für alle da, egal ob Anfängerin oder Fortgeschrittener. Mit viel Geduld und einem persönlichen Ansatz helfe ich Ihnen, Ihre musikalischen Ziele zu erreichen und die Freude an der Musik zu entdecken.
+Neben dem Unterricht lade ich Sie zu Konzerten, Workshops und CD-Projekten ein. Gerne können Sie mich auch für eigene Projekte anfragen.
+
+Schauen Sie sich in Ruhe um, und wenn Sie Fragen haben oder Interesse an einem meiner Angebote, melden Sie sich einfach. Ich freue mich darauf, von Ihnen zu hören!
+
+Herzliche Grüße,
+[Dein Name]`,
+};
