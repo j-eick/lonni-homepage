d82ae785bc69e46d12de70497c885f4a30481cb8
Joschka Eickhoff <jeickhoff@posteo.de>
2024-12-11 23:51:50 +0100

    added 2 versions of sideNav

--- components/ui/nav/desktop-design-versions/SideNavSquaresA.tsx
+++ components/ui/nav/desktop-design-versions/SideNavSquaresA.tsx
@@ -1,18 +1,16 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+type SideNavProps = {
+    className: string;
+};
+
 // SideNavSquares-Shape version A
-export default function SideNavSquaresA() {
+export default function SideNavSquaresA({ className }: SideNavProps) {
     const path = usePathname();
 
     return (
-        <aside
-            className={`bb
-                            w-full
-                            col-start-1 col-end-6
-                            row-start-2 row-end-12
-                            `}
-        >
+        <aside className={className}>
             <nav
                 className={`h-full grid grid-cols-12 grid-rows-16`}
                 id="startpage-navItems"
@@ -20,42 +18,43 @@ export default function SideNavSquaresA() {
                 <Link
                     href={"/home"}
                     className={`z-20
-                                    row-start-2 row-end-5
-                                    col-start-6 col-end-10`}
+                                col-start-6 col-end-11
+                                row-start-2 row-end-5
+                                `}
                 >
                     Willkommen!
                 </Link>
                 <Link
                     href={"/home/unterricht"}
                     className={`z-10
-                                    col-start-2 col-end-8
-                                    row-start-4 row-end-8
-                                    ${path === "unterricht" ? "bg-selected" : ""}
-                                    `}
+                                col-start-2 col-end-9
+                                row-start-4 row-end-8
+                                ${path === "unterricht" ? "bg-selected" : ""}
+                                `}
                 >
                     Unterricht
                 </Link>
                 <Link
                     href={""}
-                    className={`z-20
-                                    col-start-5 col-end-10
-                                    row-start-7 row-end-10`}
+                    className={`z-10
+                                col-start-5 col-end-10
+                                row-start-7 row-end-10`}
                 >
                     Künsterlische Tätigkeiten
                 </Link>
                 <Link
                     href={""}
                     className={`bb bg-blue-400
-                                    col-start-2 col-end-7
-                                    row-start-12 row-end-15`}
+                                col-start-2 col-end-7
+                                row-start-12 row-end-15`}
                 >
                     Auftritte & CDs
                 </Link>
                 <Link
                     href={""}
                     className={`bb bg-slate-400
-                                    col-start-5 col-end-10
-                                    row-start-14 row-end-16`}
+                                col-start-5 col-end-10
+                                row-start-14 row-end-16`}
                 >
                     Kontakt
                 </Link>
