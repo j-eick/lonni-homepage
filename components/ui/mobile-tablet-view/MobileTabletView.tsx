059f86e22cb05fd0a0d08d50df2b18c2e99674dd
Joschka Eickhoff <jeickhoff@posteo.de>
2024-12-13 04:17:30 +0100

    ui / MobileView: added Header component

--- components/ui/mobile-tablet-view/MobileTabletView.tsx
+++ components/ui/mobile-tablet-view/MobileTabletView.tsx
@@ -1,10 +1,17 @@
-import Header from "../header/Header";
+import Header from "./header/Header";
 
 export default function MobileTablet() {
     return (
-        <>
-            <Header className={`h-8 bb`}>Header</Header>
-            <main>main</main>
-        </>
+        <div className="h-full w-screen pt-14">
+            <div className="h-full w-9/10 mx-auto my-0 flex flex-col">
+                <Header className="flex w-full bb" />
+                <main
+                    className={`bg-red-300
+                                grow`}
+                >
+                    main
+                </main>
+            </div>
+        </div>
     );
 }
---
+++ components/ui/mobile-tablet-view/header/Header.tsx
@@ -0,0 +1,77 @@
+import { navLinks } from "@/lib/navLinks";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+type HeaderProps = {
+    children?: ReactNode;
+    className: string;
+};
+
+export default function Header({ className }: HeaderProps) {
+    const liStyle = "grow";
+    const linkStyle = "grid place-items-center";
+
+    return (
+        <header className={className}>
+            <ul className="w-full h-full flex justify-around">
+                <li className="grow">
+                    <ul className={`h-full flex justify-between`}>
+                        <li className={liStyle}>
+                            <Link
+                                className={`h-full w-full ${linkStyle}`}
+                                href={navLinks.home.href}
+                            >
+                                {navLinks.home.name}
+                            </Link>
+                        </li>
+                        <li className={liStyle}>
+                            <Link
+                                className={`h-full w-full ${linkStyle}`}
+                                href={navLinks.unterricht.href}
+                            >
+                                {navLinks.unterricht.name}
+                            </Link>
+                        </li>
+                        <li className={liStyle}>
+                            <Link
+                                className={`h-full w-full ${linkStyle}`}
+                                href={navLinks.kt.href}
+                            >
+                                {navLinks.kt.name}
+                            </Link>
+                        </li>
+                    </ul>
+                </li>
+                <li className="w-4/12">
+                    <h1
+                        id="titleLonni"
+                        className="grid grid-cols-10 grid-rows-2"
+                    >
+                        <span className="w-full col-start-2">Lonni</span>
+                        <span className="w-full row-start-2 col-start-4">Inman</span>
+                    </h1>
+                </li>
+                <li className="grow">
+                    <ul className={`h-full flex justify-between`}>
+                        <li className={liStyle}>
+                            <Link
+                                className={`h-full w-full leading-1.5 ${linkStyle}`}
+                                href={navLinks.auftritteCDs.href}
+                            >
+                                Auftritte <br /> und CDs
+                            </Link>
+                        </li>
+                        <li className={liStyle}>
+                            <Link
+                                className={`h-full w-full ${linkStyle}`}
+                                href={navLinks.kontakt.href}
+                            >
+                                {navLinks.kontakt.name}
+                            </Link>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </header>
+    );
+}
