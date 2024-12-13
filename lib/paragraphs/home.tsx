86dfcf18f99e04f004b4a5f94dbaf3bc7bc7eaf1
Joschka Eickhoff <jeickhoff@posteo.de>
2024-12-13 16:36:12 +0100

    fix / welcomeText: formatted

---
+++ lib/paragraphs/home.tsx
@@ -0,0 +1,23 @@
+export const welcomeText = (
+    <>
+        <p>
+            Ich freue mich, meine Leidenschaft für Musik mit Ihnen zu teilen – sei es in individuellen
+            Unterrichtsstunden, spannenden Projekten oder besonderen Konzerterlebnissen.{" "}
+        </p>
+        <p>
+            Mein Unterricht ist für alle da, egal ob Anfängerin oder Fortgeschrittener. Mit viel Geduld und einem
+            persönlichen Ansatz helfe ich Ihnen, Ihre musikalischen Ziele zu erreichen und die Freude an der Musik zu
+            entdecken. Neben dem Unterricht lade ich Sie zu Konzerten, Workshops und CD-Projekten ein. Gerne können Sie
+            mich auch für eigene Projekte anfragen.
+        </p>
+        <p>
+            Schauen Sie sich in Ruhe um, und wenn Sie Fragen haben oder Interesse an einem meiner Angebote, melden Sie
+            sich einfach. Ich freue mich darauf, von Ihnen zu hören!
+        </p>
+
+        <p>
+            Herzliche Grüße, <br />
+            Lonni Inman
+        </p>
+    </>
+);
