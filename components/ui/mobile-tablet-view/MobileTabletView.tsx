0587c53af6acb0bf9c3ce8f9ba4c3a0fc0e676e3
Joschka Eickhoff <jeickhoff@posteo.de>
2024-12-13 16:36:59 +0100

    ui / MobileTableView basic layout

--- components/ui/mobile-tablet-view/MobileTabletView.tsx
+++ components/ui/mobile-tablet-view/MobileTabletView.tsx
@@ -1,16 +1,33 @@
+import { welcomeText } from "@/lib/paragraphs/home";
 import Header from "./header/Header";
 
 export default function MobileTablet() {
     return (
         <div className="h-full w-screen pt-14">
-            <div className="h-full w-9/10 mx-auto my-0 flex flex-col">
-                <Header className="flex w-full bb" />
-                <main
-                    className={`bg-red-300
-                                grow`}
-                >
-                    main
-                </main>
+            <div className="min-h-full w-8.5/10 mx-auto my-0 flex flex-col">
+                <Header className="relative flex w-full mb-14" />
+                <section className="w-9/10 mx-auto my-0 flex gap-3 bbb">
+                    <main className={`w-9/12`}>
+                        <article className="flowText">
+                            <h1 className="relative mb-7 text-pageTitle leading-1.3">
+                                <span className="block w-11/12 border-b-1 border-slate-700/40">
+                                    <span className="font-bold text-firstLetterTitle">H</span>erzlich Willkommen!
+                                </span>
+                            </h1>
+                            <div className="bb flowText">{welcomeText}</div>
+                        </article>
+                    </main>
+                    <aside className="grow leading-1 bb">
+                        <ul>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est nemo distinctio cumque.
+                                Consequatur commodi quos suscipit consequuntur ea
+                            </li>
+                            <li>b</li>
+                            <li>c</li>
+                        </ul>
+                    </aside>
+                </section>
             </div>
         </div>
     );
