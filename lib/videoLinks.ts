a58b361379b405793486cf91410eca9e2bc5f10b
Joschka Eickhoff <jeickhoff@posteo.de>
2024-12-16 10:56:08 +0100

    content / added video links

---
+++ lib/videoLinks.ts
@@ -0,0 +1,3 @@
+export const soloFlute = "https://www.youtube.com/watch?v=is9dRWiRDNs";
+export const concert = "https://www.youtube.com/watch?v=gEUwCTkKEwM";
+export const interview = "https://www.youtube.com/watch?v=NEyrmkzSGLg";
