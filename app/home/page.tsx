b6ead9a0de1e5cfc7bc0c862715abf4e45d89a9a
Joschka Eickhoff <jeickhoff@posteo.de>
2024-12-20 17:59:44 +0100

    ui / make MobileTabletView more reusable

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,33 +1,48 @@
 "use client";
 
 import useWindowSize from "../hooks/useWindowSize";
-import { usePathname } from "next/navigation";
-import { useEffect } from "react";
-import DesktopView from "@/components/ui/desktopView/DesktopView";
 import MobileTabletView from "@/components/ui/mobile-tablet-view/MobileTabletView";
+import { welcomeText } from "@/lib/paragraphs/home";
 
 export default function Home() {
     const windowSize = useWindowSize();
 
-    const path = usePathname();
-    console.log(windowSize.width);
+    // const path = usePathname();
+    // console.log(windowSize.width);
 
-    const desktopView = windowSize.width >= 1119;
+    // const desktopView = windowSize.width >= 1119;
 
-    const tabletMobileView = windowSize.width <= 1119;
+    // const tabletMobileView = windowSize.width <= 1119;
 
-    useEffect(() => {
-        console.log(path);
-    }, []);
+    // useEffect(() => {
+    //     console.log(path);
+    // }, []);
 
     return (
-        <div
-            className={`h-full mx-auto my-0  
-                        ${windowSize.width >= 1119 ? "grid grid-cols-18 grid-rows-12" : ""}`}
-        >
+        <>
             <p className={`absolute top-2 right-2`}>{windowSize.width}px</p>
-            {desktopView && <DesktopView />}
-            {tabletMobileView && <MobileTabletView />}
-        </div>
+            <MobileTabletView>
+                <main className={`w-9/12`}>
+                    <article className="flowText">
+                        <h1 className="relative mb-7 text-pageTitle leading-1.3">
+                            <span className="block w-11/12 border-b-1 border-slate-700/40">
+                                <span className="font-bold text-firstLetterTitle">H</span>erzlich Willkommen!
+                            </span>
+                        </h1>
+                        <div className="bb pl-5 flowText">{welcomeText}</div>
+                    </article>
+                </main>
+                <aside className="grow leading-1 bb">
+                    <ul>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est nemo distinctio cumque.
+                            Consequatur commodi quos suscipit consequuntur ea
+                        </li>
+                        <li>b</li>
+                        <li>c</li>
+                    </ul>
+                </aside>
+            </MobileTabletView>
+        </>
     );
 }
--- components/ui/mobile-tablet-view/MobileTabletView.tsx
+++ components/ui/mobile-tablet-view/MobileTabletView.tsx
@@ -1,7 +1,7 @@
-import { welcomeText } from "@/lib/paragraphs/home";
 import Header from "./header/Header";
+import { ReactNode } from "react";
 
-export default function MobileTablet() {
+export default function MobileTablet({ children }: { children: ReactNode }) {
     return (
         <div className="h-full w-screen pt-14">
             <div className="min-h-full w-8.5/10 mx-auto my-0 flex flex-col">
@@ -13,28 +13,7 @@ export default function MobileTablet() {
                     link4="Auftritte & CDs"
                     link5="Kontakt"
                 />
-                <section className="w-9/10 mx-auto my-0 flex gap-3">
-                    <main className={`w-9/12`}>
-                        <article className="flowText">
-                            <h1 className="relative mb-7 text-pageTitle leading-1.3">
-                                <span className="block w-11/12 border-b-1 border-slate-700/40">
-                                    <span className="font-bold text-firstLetterTitle">H</span>erzlich Willkommen!
-                                </span>
-                            </h1>
-                            <div className="bb pl-5 flowText">{welcomeText}</div>
-                        </article>
-                    </main>
-                    <aside className="grow leading-1 bb">
-                        <ul>
-                            <li>
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est nemo distinctio cumque.
-                                Consequatur commodi quos suscipit consequuntur ea
-                            </li>
-                            <li>b</li>
-                            <li>c</li>
-                        </ul>
-                    </aside>
-                </section>
+                <section className="w-9/10 mx-auto my-0 flex gap-3">{children}</section>
             </div>
         </div>
     );
