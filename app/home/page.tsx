75ce8df41e8694ae708b4015d42d970b43d48435
Joschka Eickhoff <jeickhoff@posteo.de>
2024-12-21 13:28:46 +0100

    ui / MainLayout

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,30 +4,5 @@ import MainLayout from "@/components/ui/mainLayout/MainLayout";
 import { welcomeText } from "@/lib/paragraphs/home";
 
 export default function Home() {
-    return (
-        <>
-            <MainLayout>
-                <main className={`w-9/12`}>
-                    <article className="flowText">
-                        <h1 className="relative mb-7 text-pageTitle leading-1.3">
-                            <span className="block w-11/12 border-b-1 border-slate-700/40">
-                                <span className="font-bold text-firstLetterTitle">H</span>erzlich Willkommen!
-                            </span>
-                        </h1>
-                        <div className="pr-10 flowText">{welcomeText}</div>
-                    </article>
-                </main>
-                <aside className="grow leading-1 bb">
-                    <ul>
-                        <li>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est nemo distinctio cumque.
-                            Consequatur commodi quos suscipit consequuntur ea
-                        </li>
-                        <li>b</li>
-                        <li>c</li>
-                    </ul>
-                </aside>
-            </MainLayout>
-        </>
-    );
+    return <MainLayout>{welcomeText}</MainLayout>;
 }
--- components/ui/mainLayout/MainLayout.tsx
+++ components/ui/mainLayout/MainLayout.tsx
@@ -2,6 +2,7 @@ import useWindowSize from "@/app/hooks/useWindowSize";
 import Header from "./header/Header";
 import { ReactNode, useEffect } from "react";
 import { usePathname } from "next/navigation";
+import SideVideos from "../SideVideos";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
     const windowSize = useWindowSize();
@@ -24,9 +25,38 @@ export default function MainLayout({ children }: { children: ReactNode }) {
                     link4="Auftritte & CDs"
                     link5="Kontakt"
                 />
-
                 {/* vvv---vvv PER PAGE CONTENT GOES HERE vvv---vvv */}
-                <section className="w-9/10 mx-auto my-0 flex gap-5">{children}</section>
+                <section
+                    className={`grow 
+                                w-9/10 mx-auto my-0 flex gap-5`}
+                >
+                    <main className={`w-9/12`}>
+                        <article className="flowText">
+                            <h1 className="relative mb-7 text-pageTitle leading-1.3">
+                                <span className="block w-11/12 border-b-1 border-slate-700/40">
+                                    <span className="font-bold text-firstLetterTitle">H</span>erzlich Willkommen!
+                                </span>
+                            </h1>
+                            <div className="pr-10 flowText">{children}</div>
+                        </article>
+                    </main>
+                    <SideVideos />
+                </section>
+                <div
+                    className={`bb 
+                                w-9/10 mx-auto my-0 mt-5
+                                flex `}
+                >
+                    <ul
+                        className={`bb w-1/3 h-full p-2 flex justify-between gap-5
+                                    bg-slate-300 `}
+                    >
+                        <li className="grow text-center">Unterricht</li>
+                        <li className="grow text-center">Konzerte</li>
+                        <li className="grow text-center">Kontakt</li>
+                    </ul>
+                    <div />
+                </div>
             </div>
         </div>
     );
