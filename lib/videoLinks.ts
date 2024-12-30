d2bd984f5f2cfc67c4b58864728e69b02449acf9
Joschka Eickhoff <jeickhoff@posteo.de>
2024-12-30 17:46:39 +0100

    allVideoLinks.ts

--- lib/videoLinks.ts
+++ lib/videoLinks.ts
@@ -1,3 +1,6 @@
 export const soloFlute = "https://www.youtube.com/watch?v=is9dRWiRDNs";
 export const concert = "https://www.youtube.com/watch?v=gEUwCTkKEwM";
 export const interview = "https://www.youtube.com/watch?v=NEyrmkzSGLg";
+
+type allVideoLinksProps = string[];
+export const allVideoLinks: allVideoLinksProps = [soloFlute, concert, interview];
