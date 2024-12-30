b066b14c7f3ba6ce89285f1393f1862a5f6de68d
Joschka Eickhoff <jeickhoff@posteo.de>
2024-12-30 17:42:01 +0100

    MV paedagogische Erfahrung unterricht

--- app/home/unterricht/page.tsx
+++ app/home/unterricht/page.tsx
@@ -1,16 +1,26 @@
 "use client";
 
-import useWindowSize from "@/app/hooks/useWindowSize";
+import Header from "@/components/ui/mainLayout/header/Header";
 import MainLayout from "@/components/ui/mainLayout/MainLayout";
+import SideVideos from "@/components/ui/SideVideos";
 import { unterricht } from "@/lib/paragraphs/unterricht";
 
 export default function Home() {
-    const windowSize = useWindowSize();
-
     return (
-        <>
-            <p className={`absolute top-2 right-2`}>{windowSize.width}px</p>
-            <MainLayout>
+        <MainLayout>
+            <Header
+                className="relative flex w-full mb-14"
+                link1="Home"
+                link2="Unterricht"
+                link3="Projekte"
+                link4="Auftritte & CDs"
+                link5="Kontakt"
+            />
+            {/* vvv---vvv PER PAGE CONTENT GOES HERE vvv---vvv */}
+            <section
+                className={`grow 
+                w-9/10 mx-auto my-0 flex gap-5`}
+            >
                 <main className={`w-9/12`}>
                     <article className="flowText">
                         <h1 className="relative mb-7 text-pageTitle leading-1.3">
@@ -18,20 +28,26 @@ export default function Home() {
                                 <span className="font-bold text-firstLetterTitle">U</span>nterricht
                             </span>
                         </h1>
-                        <div className="flowText br">{unterricht}</div>
+                        <div className="pr-10 flowText">{unterricht}</div>
                     </article>
                 </main>
-                <aside className="grow leading-1 bb">
-                    <ul>
-                        <li>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est nemo distinctio cumque.
-                            Consequatur commodi quos suscipit consequuntur ea
-                        </li>
-                        <li>b</li>
-                        <li>c</li>
-                    </ul>
-                </aside>
-            </MainLayout>
-        </>
+                <SideVideos />
+            </section>
+            {/* <div
+                className={`bb 
+                w-9/10 mx-auto my-0 mt-5
+                flex `}
+            >
+                <ul
+                    className={`bb w-1/3 h-full p-2 flex justify-between gap-5
+                    bg-slate-300 `}
+                >
+                    <li className="grow text-center">Unterricht</li>
+                    <li className="grow text-center">Konzerte</li>
+                    <li className="grow text-center">Kontakt</li>
+                </ul>
+                <div />
+            </div> */}
+        </MainLayout>
     );
 }
