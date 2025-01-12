4a0a61ab3b1c2924a2da0ca831509a624c534766
Joschka Eickhoff <jeickhoff@posteo.de>
2025-01-12 01:49:35 +0100

    Banner setup for later implementation

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,14 +4,15 @@ import Header from "@/components/ui/mainLayout/header/Header";
 import MainLayout from "@/components/ui/mainLayout/MainLayout";
 import PageTitle from "@/components/ui/page-title/PageTitle";
 import { welcomeText } from "@/lib/paragraphs/home";
+// import styles from "./MovingBanner.module.css";
 
 export default function Home() {
     return (
         <MainLayout>
             <Header className="relative flex w-full mb-14" />
-            {/* vvv---vvv PER PAGE CONTENT GOES HERE vvv---vvv */}
+            {/* ---vvv---vvv--- PER PAGE CONTENT GOES HERE ---vvv---vvv--- */}
             <section
-                className={`grow 
+                className={`grow
                 w-9/10 mx-auto my-0 flex gap-5`}
             >
                 <main className={`w-9/12`}>
@@ -26,21 +27,9 @@ export default function Home() {
                     </article>
                 </main>
             </section>
-            <div
-                className={`bb 
-                w-9/10 mx-auto my-0 mt-5
-                flex `}
-            >
-                <ul
-                    className={`bb w-1/3 h-full p-2 flex justify-between gap-5
-                    bg-slate-300 `}
-                >
-                    <li className="grow text-center">Unterricht</li>
-                    <li className="grow text-center">Konzerte</li>
-                    <li className="grow text-center">Kontakt</li>
-                </ul>
-                {/* <div /> */}
-            </div>
+            {/* <div className={`bb ${styles.bannerContainer}`}>
+                <p className={styles.movingBanner}>This is information</p>
+            </div> */}
         </MainLayout>
     );
 }
--- components/ui/mainLayout/MainLayout.tsx
+++ components/ui/mainLayout/MainLayout.tsx
@@ -1,13 +1,19 @@
 import useWindowSize from "@/app/hooks/useWindowSize";
 import { ReactNode } from "react";
+// import styles from "./MovingBanner.module.css";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
     const windowSize = useWindowSize();
 
     return (
-        <div className="h-full w-screen pt-14">
-            <p className={`absolute top-2 right-2`}>{windowSize.width}px</p>
-            <div className="min-h-full w-8.5/10 max-w-[1273px] mx-auto my-0 flex flex-col">{children}</div>
+        <div className="h-full w-screen relative">
+            {/* <div className={`absolute top-10 bb ${styles.bannerContainer}`}>
+                <p className={styles.movingBanner}>This is information</p>
+            </div> */}
+            <div className="w-screen pt-10">
+                <p className={`absolute top-2 right-2`}>{windowSize.width}px</p>
+                <div className="min-h-full w-8.5/10 max-w-[1273px] mx-auto my-0 flex flex-col">{children}</div>
+            </div>
         </div>
     );
 }
