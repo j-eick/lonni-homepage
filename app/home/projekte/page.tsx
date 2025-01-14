3d0f1d56bb76d21f0777ca5cf1864bf7eee3eb23
Joschka Eickhoff <jeickhoff@posteo.de>
2025-01-14 21:54:16 +0100

    clean up

--- app/home/projekte/page.tsx
+++ app/home/projekte/page.tsx
@@ -3,6 +3,7 @@
 import Header from "@/components/ui/mainLayout/header/Header";
 import MainLayout from "@/components/ui/mainLayout/MainLayout";
 import { projekte } from "@/lib/paragraphs/projekte";
+import "../../globals.css";
 
 export default function Home() {
     return (
@@ -13,7 +14,7 @@ export default function Home() {
                 className={`grow 
                 w-9/10 mx-auto my-0 flex gap-5`}
             >
-                <main className={`w-9/12`}>
+                <main className={`w-full br`}>
                     <article className="flowText">
                         <h1 className="relative mb-7 text-pageTitle leading-1.3">
                             <span
@@ -23,9 +24,25 @@ export default function Home() {
                                 <span className="font-bold text-firstLetterTitle">P</span>rojekte
                             </span>
                         </h1>
-                        <div className="pr-10 flowText">{projekte}</div>
+                        <div>{projekte}</div>
                     </article>
                 </main>
+                {/* <aside
+                    className={`bb
+                                w-3/12`}
+                >
+                    <ul
+                        className={`mb-3 w-full
+                                    flex flex-col gap-5 justify-around items-center font-pagelinks tracking-wider text-xl`}
+                    >
+                        <li className="relative w-10/12 h-[200px] overflow-hidden thumbnail-link">
+                            <span className="absolute left-1/2 -translate-x-1/2 bottom-3">www.trioclassico.de</span>
+                        </li>
+                        <li className="relative w-10/12 h-[200px] overflow-hidden thumbnail-link">
+                            <span className="absolute left-1/2 -translate-x-1/2 bottom-3">www.ilclavifiato.de</span>
+                        </li>
+                    </ul>
+                </aside> */}
             </section>
         </MainLayout>
     );
