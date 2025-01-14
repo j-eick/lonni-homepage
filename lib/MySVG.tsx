baa7ce636c92c9655fcaa4ecd3c67fca391b5c17
Joschka Eickhoff <jeickhoff@posteo.de>
2025-01-14 21:54:22 +0100

    ui / added icon

--- lib/MySVG.tsx
+++ lib/MySVG.tsx
@@ -4,6 +4,7 @@ type ItemProp = (props: { size?: number }) => ReactNode;
 
 type SVGProps = {
     BulletItemArrow: ItemProp;
+    ArrowRight: ItemProp;
 };
 
 export const MySVG: SVGProps = {
@@ -22,6 +23,16 @@ export const MySVG: SVGProps = {
                 strokeWidth={2}
                 d="M13 7H7l4 5l-4 5h6l4-5z"
             ></path>
+        </svg>
+    ),
+    ArrowRight: ({ size = 20 }) => (
+        <svg
+            width={size}
+            height={size}
+            fill="currentColor"
+            viewBox="0 0 16 16"
+        >
+            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
         </svg>
     ),
 };
