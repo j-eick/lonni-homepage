a2579160e22998d9b49883c1a465386e325517a8
Joschka Eickhoff <jeickhoff@posteo.de>
2025-01-17 09:40:09 +0100

    fix / PageTitle

--- components/ui/page-title/PageTitle.tsx
+++ components/ui/page-title/PageTitle.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from "react";
 
 type PageTitleProps = {
-    children?: ReactNode;
+    content?: ReactNode;
 };
-export default function PageTitle({ children }: PageTitleProps) {
+export default function PageTitle({ content }: PageTitleProps) {
     return (
         <h1 className="relative mb-7 text-pageTitle leading-1.3">
-            {children}
+            {content}
             <span
                 className={`block font-pageTitle tracking-wider
                                             w-11/12 border-b-1 border-slate-700/40`}
