ab99f942d3914871af88cb78f577e0f878612b9e
Joschka Eickhoff <jeickhoff@posteo.de>
2025-01-18 17:56:52 +0100

    for disclaimer /  Icons8.com

--- lib/paragraphs/kontakt.tsx
+++ lib/paragraphs/kontakt.tsx
@@ -33,5 +33,30 @@ export const kontakt = (
             </span>
             <p>Adresse: Beethovenallee 14 22529 Hamburg</p>
         </div>
+
+        <div>Website gestaltet und umgesetzt von Joschka Eickhoff</div>
+        <div>
+            <p className="border-b-1 border-slate-700">Verwendete Hilfmittel:</p>
+            <ul>
+                <li>
+                    <h1 className="text-size-default font-semibold">icons8.com</h1>
+                    <p>
+                        <a
+                            target="_blank"
+                            href="https://icons8.com/icon/3we6QhkgvXBB/right"
+                        >
+                            Right
+                        </a>{" "}
+                        icon by{" "}
+                        <a
+                            target="_blank"
+                            href="https://icons8.com"
+                        >
+                            Icons8
+                        </a>
+                    </p>
+                </li>
+            </ul>
+        </div>
     </>
 );
