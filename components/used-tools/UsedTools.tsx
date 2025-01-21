23aec422f7c3dae41f711616179bd3fa331d091c
Joschka Eickhoff <jeickhoff@posteo.de>
2025-01-21 12:19:37 +0100

    this will become a list of used tools

---
+++ components/used-tools/UsedTools.tsx
@@ -0,0 +1,27 @@
+export default function UsedTools() {
+    return (
+        <div>
+            <p className="border-b-1 border-slate-700">Verwendete Hilfmittel:</p>
+            <ul>
+                <li>
+                    <h1 className="text-size-default font-semibold">icons8.com</h1>
+                    <p>
+                        <a
+                            target="_blank"
+                            href="https://icons8.com/icon/3we6QhkgvXBB/right"
+                        >
+                            Right
+                        </a>{" "}
+                        icon by{" "}
+                        <a
+                            target="_blank"
+                            href="https://icons8.com"
+                        >
+                            Icons8
+                        </a>
+                    </p>
+                </li>
+            </ul>
+        </div>
+    );
+}
