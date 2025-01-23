c429c8b453c819fa487ff0960171e2b288d3c0d4
Joschka Eickhoff <jeickhoff@posteo.de>
2025-01-23 09:40:08 +0100

    failed attempt / use thumbnails to cover up yt previews;

--- components/ui/SideVideos.tsx
+++ components/ui/SideVideos.tsx
@@ -1,4 +1,6 @@
 import { allVideoLinks } from "@/lib/videoLinks";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 type SideVideosProps = {
     width?: number;
@@ -6,19 +8,65 @@ type SideVideosProps = {
 };
 
 export default function SideVideos({ width = 290, height = 155 }: SideVideosProps) {
+    const [hoveredThumbnail, setHoveredThumbnail] = useState<string>("");
+
+    useEffect(() => {
+        console.log(hoveredThumbnail);
+    }, [hoveredThumbnail]);
+
     return (
-        <aside className="grow p-5  h-max">
-            <ul className={`flex flex-col gap-5`}>
+        <aside className={`grow h-max pb-10 flex justify-end`}>
+            <ul
+                className={`p-5 mx-auto my-0 flex flex-col items-end gap-7 
+                             `}
+            >
                 {allVideoLinks.map(link => (
                     <li
-                        key={link}
-                        className="w-max overflow-hidden rounded-3xl border-white"
+                        key={link.url}
+                        className={`relative w-max mx-auto my-0 rounded-lg overflow-hidden  
+                                    transition-all duration-300
+                                    ${
+                                        hoveredThumbnail === link.thumbnail
+                                            ? "saturate-100 opacity-100 border-1 border-slate-200 shadow-step"
+                                            : "saturate-50 opacity-70 border-1 border-slate-700 shadow-flat"
+                                    }
+                                  `}
+                        onMouseEnter={() => setHoveredThumbnail(link.thumbnail)}
+                        onMouseLeave={() => setHoveredThumbnail("")}
                     >
                         <iframe
-                            src={link}
+                            src={link.url}
                             width={width}
                             height={height}
+                            allowFullScreen
                         />
+                        {/* ---------------------------------------- */}
+                        {/* -----  Cover that hides Thumbnail  ----- */}
+                        {/* ---------------------------------------- */}
+                        {/* <div
+                            className={`absolute z-10 inset-0 transition-all duration-500 ease-in-out
+                                        ${hoveredThumbnail === link.thumbnail ? "opacity-0" : ""}
+                                        `}
+                            // onMouseEnter={() => setHoveredThumbnail(link.thumbnail)}
+                            // onMouseLeave={() => setHoveredThumbnail("")}
+                        >
+                            <div className="relative">
+                                <Image
+                                    src={link.thumbnail}
+                                    width={width}
+                                    height={height}
+                                    alt={""}
+                                />
+                                <div
+                                    className={`absolute bottom-1.5 w-full h-14
+                                                grid place-items-center 
+                                                bg-slate-800/70 text-slate-200 font-secondary tracking-wider
+                                                ${link.id === 2 ? "text-size-" : ""}`}
+                                >
+                                    {link.title}
+                                </div>
+                            </div>
+                        </div> */}
                     </li>
                 ))}
             </ul>
--- lib/videoLinks.ts
+++ lib/videoLinks.ts
@@ -1,6 +1,26 @@
-export const soloFlute = "https://www.youtube.com/embed/is9dRWiRDNs";
-export const concert = "https://www.youtube.com/embed/gEUwCTkKEwM";
-export const interview = "https://www.youtube.com/embed/NEyrmkzSGLg";
+export const soloFlute = {
+    id: 1,
+    url: "https://www.youtube.com/embed/is9dRWiRDNs",
+    thumbnail: "/thumbnails/thumbnail1.jpg",
+    title: "Flöte mal historisch",
+};
+export const concert = {
+    id: 2,
+    url: "https://www.youtube.com/embed/gEUwCTkKEwM",
+    thumbnail: "/thumbnails/thumbnail2.jpg",
+    title: "Il Clavifiato - Historische Instrumente",
+};
+export const interview = {
+    id: 3,
+    url: "https://www.youtube.com/embed/NEyrmkzSGLg",
+    thumbnail: "/thumbnails/thumbnail3.jpg",
+    title: "Zauberstab Flöte beendet Schumann Plus Reihe",
+};
 
-type allVideoLinksProps = string[];
-export const allVideoLinks: allVideoLinksProps = [soloFlute, concert, interview];
+type allVideoLinksProps = {
+    id: number;
+    url: string;
+    thumbnail: string;
+    title: string;
+};
+export const allVideoLinks: allVideoLinksProps[] = [soloFlute, concert, interview];
