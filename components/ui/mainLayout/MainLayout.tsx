13cf157b339f52262a67dcab9d9267888dd88c50
Joschka Eickhoff <jeickhoff@posteo.de>
2025-02-20 13:36:38 +0100

    ui / prepared marquee

--- components/ui/mainLayout/MainLayout.tsx
+++ components/ui/mainLayout/MainLayout.tsx
@@ -1,15 +1,10 @@
-/**
- * The <div> beneath the header is solely to push the main content below
- * the floating header.
- */
-
 import useWindowSize from "@/app/hooks/useWindowSize";
 import { Dispatch, ReactNode, SetStateAction, useEffect, useState } from "react";
 import Header from "./header/Header";
 import { navLinksLeft, navLinksRight } from "../../../lib/navLinks.ts";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-// import styles from "./MovingBanner.module.css";
+import "../../../app/globals.css";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
     const path = usePathname();
@@ -39,38 +34,86 @@ export default function MainLayout({ children }: { children: ReactNode }) {
         };
     }, [isModalOpen]);
 
+    useEffect(() => {
+        const marquee = document.querySelector<HTMLDivElement>(".banner");
+        let offset: number;
+
+        if (!marquee) {
+            console.error("Element with class .text-marque not found");
+            return;
+        }
+
+        console.log(marquee.offsetWidth);
+
+        offset = marquee.offsetWidth;
+
+        function animateMarquee() {
+            offset -= 2;
+
+            if (!marquee) {
+                console.error("marquee not found");
+                return;
+            }
+            marquee.style.transform = `translateX(${offset}px)`;
+
+            if (Math.abs(offset) > marquee.scrollWidth) {
+                offset = marquee.offsetWidth;
+            }
+
+            requestAnimationFrame(animateMarquee);
+        }
+
+        animateMarquee();
+    }, []);
+
     return (
         <div
-            className={`relative h-full w-screen no-scrollbar overflow-auto
+            className={`relative min-h-full w-screen no-scrollbar overflow-auto
+                        font-geosans text-size-default text-color-primary
                          md:pt-5 md:pb-20`}
         >
             <p className={`absolute top-2 right-2`}>{windowSize.width}px</p>
 
             <BackdropLayer isModalOpen={isModalOpen} />
+
             <Modal
                 isModalOpen={isModalOpen}
                 setIsModalOpen={setIsModalOpen}
                 underlineLinkClasses={underlineLinkClasses}
             />
-            {/* <div className="absolute top-0 left-0 w-screen h-11 bg-gradient-to-r from-transparent via-gray-400/20 to-transparent border-t border-b border-gray-200/50 overflow-hidden">
-                <div className="whitespace-nowrap animate-marquee">asd</div>
-            </div> */}
-
-            <div
-                className={`min-h-full max-w-canvas--1270px overflow-hidden
+            <div className="relative ">
+                <Canvas
+                    className={`relative max-w-canvas--1270px overflow-hidden
+                    mb-12 mx-auto my-0
+                    md:w-8.5/10`}
+                >
+                    <Header
+                        isModalOpen={isModalOpen}
+                        setIsModalOpen={setIsModalOpen}
+                        buttonClicked={buttonClicked}
+                        underlineLinkClasses={underlineLinkClasses}
+                    />
+                </Canvas>
+                <div
+                    className={`
+                                absolute bottom-0  left-0 w-full h-[2.5vw] flex items-center justify-center
+                                bg-gradient-to-r from-cyan-700/10 via-purple-400/10 to-cyan-700/10 whitespace-nowrap
+                                border-[.5px] border-b-[.5px] border-white/20
+                                backdrop-blur-sm shadow-lg`}
+                >
+                    <div className="banner w-9/12 mx-6 text-size-default font-semibold text-white drop-shadow-md">
+                        {/* <span>[Moving Text Banner]</span> */}
+                    </div>
+                    {/* <div className="w-2/12 text-size-default font-semibold text-white drop-shadow-md">[Playing Track]</div> */}
+                </div>
+            </div>
+            <Canvas
+                className={`max-w-canvas--1270px overflow-hidden
                             mx-auto my-0 flex flex-col
-                            font-geosans text-size-default text-color-primary
                             md:w-8.5/10`}
             >
-                <Header
-                    isModalOpen={isModalOpen}
-                    setIsModalOpen={setIsModalOpen}
-                    buttonClicked={buttonClicked}
-                    underlineLinkClasses={underlineLinkClasses}
-                />
-                {/* -----  TOP MARGIN (pushes content below header, if viewport < 768px)  ----- */}
                 <div className="mobile-topMargin:mt-[15vw]">{children}</div>
-            </div>
+            </Canvas>
         </div>
     );
 }
@@ -118,4 +161,8 @@ function Modal({
 
 function BackdropLayer({ isModalOpen }: { isModalOpen: boolean }) {
     return <>{isModalOpen && <div className="fixed inset-0 z-20 backdrop-blur-[7px] backdrop-brightness-80" />}</>;
+}
+
+function Canvas({ children, className }: { children: ReactNode; className: string }) {
+    return <div className={className}>{children}</div>;
 }
