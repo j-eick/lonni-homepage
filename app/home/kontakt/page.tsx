bde056e2b0ecc5d4d75aa3696c9c6281b8c7c780
Joschka Eickhoff <jeickhoff@posteo.de>
2025-03-07 17:51:16 +0100

    fixed mobile / desktop view

--- app/home/kontakt/page.tsx
+++ app/home/kontakt/page.tsx
@@ -7,29 +7,30 @@ import Bulletpoint from "@/components/ui/bulletpoint/Bulletpoint";
 import Image from "next/image";
 import catImage from "../../../public/decoration/catSilhuette.svg";
 import MyImage from "@/components/image/MyImage";
-// import UsedTools from "@/components/used-tools/UsedTools";
 
 export default function Home() {
     return (
         <MainLayout>
-            {/* vvv---vvv PER PAGE CONTENT GOES HERE vvv---vvv */}
+            {/* ---  DESKTOP  --- */}
             <main
                 className={`grow
-                            w-9/10 h-full mx-auto my-0 py-20
-                            flex justify-center gap-5 `}
+                            w-full mx-auto my-0 py-20
+                            flex justify-center gap-5 
+                            2xs:hidden
+                            2sm:block `}
             >
                 <div
-                    className={`relative flowText 
+                    className={`relative mx-auto flowText
                                 rounded-lg shadow-soft kontakPage-heavyRain
-                                2xs:hidden
-                                2sm:block`}
+                                max-w-[520px]
+                                2sm-455:w-8/10
+                                `}
                 >
                     <MyImage
                         src={catImage}
-                        alt={"decorative-element"}
-                        className={`absolute right-0 -scale-x-100
-                                    2xs:w-cat-responsive 
-                                    lg-1024:w-[230px] lg-1024:-top-[3vw]`}
+                        alt={"cat-silhuette-image"}
+                        className={`absolute right-0 -scale-x-100 top-catSilhouette
+                                    2sm-455:w-catSilhuette-desktop`}
                     />
                     <div className="pt-24 pb-16 px-12">
                         <div
@@ -37,9 +38,60 @@ export default function Home() {
                                         font-secondary`}
                         >
                             <h1 className="mb-8 text-size-regularTitle border-b-1 border-color-primary">
-                                Für Anfragen & Buchung
+                                Für Anfragen & Buchungen
                             </h1>
                             <div className="flex gap-5 pl-5">
+                                <ul className="flex flex-col gap-2">
+                                    <Bulletpoint>
+                                        <strong>Telefon</strong>
+                                    </Bulletpoint>
+                                    <Bulletpoint>
+                                        <strong>Mobile</strong>
+                                    </Bulletpoint>
+                                    <Bulletpoint>
+                                        <strong>E-Mail</strong>
+                                    </Bulletpoint>
+                                    <Bulletpoint>
+                                        <strong>Adresse</strong>
+                                    </Bulletpoint>
+                                </ul>
+                                <div className="flex flex-col gap-2">
+                                    <span>040/56 32 01</span>
+                                    <span>(+49)162 332 3355</span>
+                                    <span>lonni.inman@gmx.de</span>
+                                    <span>
+                                        Beethovenallee 14 <br /> 22529 Hamburg
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            {/* ---  MOBILE @455px  --- */}
+            <main
+                className={`relative grow
+                            w-full mx-auto my-0
+                            flex justify-center gap-5 
+                            2xs:block
+                            2sm:hidden `}
+            >
+                <div className="w-11/12 mx-auto">
+                    <MyImage
+                        src={catImage}
+                        alt={"decorative-element"}
+                        className={`absolute right-0 -scale-x-100 -top-5
+                                2xs:w-catSilhuette-mobile `}
+                    />
+                    <div className={`w-max mx-auto pt-24 pb-16 px-[10vw]`}>
+                        <div
+                            className={`mx-auto my-0 flex flex-col
+                                    font-secondary`}
+                        >
+                            <h1 className="mb-5 text-size-regularTitle border-b-1 border-color-primary w-max">
+                                Für Anfragen & Buchung
+                            </h1>
+                            <div className="flex gap-5">
                                 <ul className="flex flex-col gap-2">
                                     <Bulletpoint>
                                         <strong>Telefon</strong>
